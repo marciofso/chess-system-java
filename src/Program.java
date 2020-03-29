794e01eac321e7e8f042601f414e4929a8dcdbfd
Marcio Fernandes <marciofso@hotmail.com>
2020-03-29 16:34:31 -0300

    First class: Position

--- src/Program.java
+++ src/Program.java
@@ -1,9 +1,12 @@
+import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hell World !");
+		Position pos = new Position(3,5);
+		
+		System.out.println(pos);
 
 	}
 
